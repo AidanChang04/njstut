fb1c4c2fba4bc04a56fd34e2ef378cedb5ba643e
AIDAN CHANG WAI YUE <TP063411@mail.apu.edu.my>
2023-12-10 00:04:06 +0800

    commit

--- components/AchievementsSection.jsx
+++ components/AchievementsSection.jsx
@@ -19,10 +19,11 @@ const AchievementsSection = () => {
     return(
         <div className=" xl:gap-3 sm:py-3 xl:px-12">
             <div className="border-[#33353F] border rounded-md py-8 px-16 flex flex-row items-center justify-between">
-            {achievementsList.map((achievement, index) => {
+            {achievementsList.map((achievement) => {
                 return(
                     <div 
-                    key={index} className="flex flex-col items-center justify-center mx-4">
+                    // eslint-disable-next-line react/jsx-key
+                    className="flex flex-col items-center justify-center mx-4">
                         <h2 className="text-white text-4xl font-bold">
                             {achievement.value}
                         </h2>
