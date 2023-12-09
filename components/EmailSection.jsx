f19be03d8e3ece29d6b9c0323ea75176451c3cb6
AIDAN CHANG WAI YUE <TP063411@mail.apu.edu.my>
2023-12-09 23:51:15 +0800

    commit

--- components/EmailSection.jsx
+++ components/EmailSection.jsx
@@ -14,9 +14,9 @@ const EmailSection = () => {
             <div className="z-10">
                 <h5 className="text-4xl font-bold text-white my-2 ">Let's connect! :)</h5>
                 <p className="text-[#ADB7BE] mb-4 max-w-md mt-8 text-justify">
-                As a passionate learner who loves challenges, I am excited for the opportunities and lessons that I will go through in life. However anything goes, let's remember that we always will have a place in the stars!
+                As a passionate learner who loves challenges, I am excited for the opportunities and lessons that I will go through in life. However anything goes, let&apos;s remember that we always will have a place in the stars!
                 </p>
-                <h4 className="pb-3 mt-6">Intrigued? Let's get in touch!</h4>
+                <h4 className="pb-3 mt-6">Intrigued? Let&apos;s get in touch!</h4>
                 <div className=" flex flex-row gap-4 mt-6">
                     <Link href="https://github.com/AidanChang04">
                         <Image src={GithubIcon} alt="Github Icon" />
@@ -25,10 +25,10 @@ const EmailSection = () => {
                         <Image src={LinkedinIcon} alt="Linkedin Icon" />
                     </Link>
                     <Link href="https://www.instagram.com/04___ac/" className="mt-1">
-                        <Image src={InstagramIcon}/>
+                        <Image src={InstagramIcon} alt = "Instagram Icon"/>
                     </Link>
                     <Link href="mailto:aidanchang2004@gmail.com" className="mt-1">
-                        <Image src={GmailIcon}/>
+                        <Image src={GmailIcon} alt = "Gmail Icon"/>
                     </Link>
 
                 </div>
